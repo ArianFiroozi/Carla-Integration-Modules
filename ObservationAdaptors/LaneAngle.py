1735e3e65e6a66683bf92394bed8bfa68e5773c3
ArianFiroozi <arianfiroozi3@gmail.com>
2024-12-12 16:41:31 +0330

    add: heading angle observation

---
+++ ObservationAdaptors/LaneAngle.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def get_lane_angle(ego_vehicle):
+    """
+    Get lane angle relative to ego vehicle heading 
+
+    Args:
+        ego_vehicle: The ego vehicle actor.
+    
+    Returns:
+        float: relative angle between road and vehicle heading in radians
+    """
+
+    ego_transform = ego_vehicle.get_transform()
+    ego_location = ego_vehicle.get_location()
+    ego_waypoint = map.get_waypoint(ego_location)
+
+    lane_vector = ego_waypoint.transform.get_forward_vector()
+    road_angle = np.arctan2(lane_vector.y, lane_vector.x)
+
+    car_forward_vector = ego_transform.get_forward_vector()
+    car_heading = np.arctan2(car_forward_vector.y, car_forward_vector.x)
+    
+    relative_angle = car_heading - road_angle
+    return relative_angle
