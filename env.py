7e0c69bff83dd1dd1b5efa16a90e1b710af796fa
ArianFiroozi <arianfiroozi3@gmail.com>
2025-02-22 16:31:35 +0330

    feat: checkpoint load
    loades from checkpoint
    saves to checkpoint folders

--- env.py
+++ env.py
@@ -199,6 +199,35 @@ class CarlaEnv(gymnasium.Env):
 
 from stable_baselines3 import SAC
 from stable_baselines3.common.env_checker import check_env
+import os
+
+def create_checkpoints_folder(base_path='./checkpoints/checkpoint'):
+    folder_index = 0
+    while True:
+        folder_name = f"{base_path}{folder_index}"
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+            return folder_name
+        folder_index += 1
+
+def get_latest_checkpoint(base_path='./checkpoints/checkpoint'):
+    folder_index = 0
+    folder_name=""
+    while True:
+        folder_name = f"{base_path}{folder_index}"
+        if not os.path.exists(folder_name):
+            break
+        folder_index += 1
+    
+    file_num=1
+    while True:
+        file_name = f"{base_path}{folder_index-1}/ppo_carla_checkpoint_{file_num}000_steps.zip"
+        if not os.path.exists(file_name):
+            break
+        file_num += 1
+    if file_num==1 and folder_index==0:
+        return ""
+    return f"{base_path}{folder_index-1}/ppo_carla_checkpoint_{file_num-1}000_steps.zip"
 
 def run(map_path, walkers_count, vehicles_count, steps, device, init_speed):
     env = CarlaEnv(map_path, walkers_count, vehicles_count, max_steps=steps, init_speed=init_speed)
@@ -207,15 +236,17 @@ def run(map_path, walkers_count, vehicles_count, steps, device, init_speed):
     env = DummyVecEnv([lambda: env])
     model_path = "ppo_carla_model"
 
-    if os.path.exists(model_path + ".zip"):
-        print("Loading existing model...")
-        model = PPO.load(model_path, env=env, n_epochs=10)
+    latest_checkpoint=get_latest_checkpoint()
+    if latest_checkpoint != "":
+        print(f"Loading existing model: {latest_checkpoint}")
+        model = PPO.load(latest_checkpoint, verbose=2, env=env, n_epochs=10)
     else:
         print("Creating new model...")
         model = PPO("MultiInputPolicy", env, verbose=2, tensorboard_log="./ppo_carla/", n_epochs=10)
     
     try:
-        checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./checkpoints/', name_prefix='ppo_carla_checkpoint')
+        checkpoints_folder = create_checkpoints_folder()
+        checkpoint_callback = CheckpointCallback(save_freq=1000, save_path=checkpoints_folder, name_prefix='ppo_carla_checkpoint')
 
         # model = PPO("MultiInputPolicy", env, verbose=2, tensorboard_log="./ppo_carla/")
         model.learn(total_timesteps=steps, callback=checkpoint_callback)
